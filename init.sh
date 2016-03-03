2adba6e6ec9926f57c580ab78b7cfa6662e799f5
Varun Venkataraman <vvenkata@adobe.com>
2016-03-04 01:10:17 +1100

    Major refactoring to make sure that all code is unto date for WeTelco Mobile On Demand and Labsetup

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "************* L322 - Start Initialisation ***********"
+cd /Users/vvenkata/Documents/Varun/PresentationInventory/Presentation/Summit2016/LabMachine/Users/l322/Desktop/AEM-Commerce-Lab-WS/aem-commerce-omni-channel
 cp -rf external/* ~/.m2/respository
 echo "************* L322 - End Initialisation  ***********"
