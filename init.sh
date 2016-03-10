b5a59209d41e4b07c06b66449026deee32ec5458
Varun Venkataraman <vvenkata@adobe.com>
2016-03-10 17:37:08 +1100

    Master -> Lab0

--- init.sh
+++ init.sh
@@ -2,5 +2,8 @@
 
 echo "************* L322 - Start Initialisation ***********"
 cd /Users/vvenkata/Documents/Varun/PresentationInventory/Presentation/Summit2016/LabMachine/Users/l322/Desktop/AEM-Commerce-Lab-WS/aem-commerce-omni-channel
+git fetch
+git checkout lab0
 cp -rf external/* ~/.m2/repository
+mvn clean install
 echo "************* L322 - End Initialisation  ***********"
