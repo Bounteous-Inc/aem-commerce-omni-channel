5883014f0183e7d34fb0615cccd2e80c370db2d1
Varun Venkataraman <vvenkata@adobe.com>
2016-03-12 08:59:38 +1100

    Switching from Master to lab0

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "************* L322 - Start Initialisation ***********"
-cd /Users/vvenkata/Documents/Varun/PresentationInventory/Presentation/Summit2016/LabMachine/Users/l322/Desktop/AEM-Commerce-Lab-WS/aem-commerce-omni-channel
+# cd /Users/vvenkata/Documents/Varun/PresentationInventory/Presentation/Summit2016/LabMachine/Users/l322/Desktop/AEM-Commerce-Lab-WS/aem-commerce-omni-channel
 git fetch
 git checkout lab0
 cp -rf external/* ~/.m2/repository
