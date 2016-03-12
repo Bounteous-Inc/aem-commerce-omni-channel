b82c35482a0c6763f715cd174eeef0ad2c1cb622
Varun Venkataraman <vvenkata@adobe.com>
2016-03-12 18:07:32 +1100

    Renaming Lab0 - Lab1

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 echo "************* L322 - Start Initialisation ***********"
 # cd /Users/vvenkata/Documents/Varun/PresentationInventory/Presentation/Summit2016/LabMachine/Users/l322/Desktop/AEM-Commerce-Lab-WS/aem-commerce-omni-channel
 git fetch
-git checkout lab0
+git checkout lab1
 cp -rf external/* ~/.m2/repository
 mvn clean install -PautoInstallPackage
 echo "************* L322 - End Initialisation  ***********"
