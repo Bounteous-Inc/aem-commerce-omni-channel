6544617f1a5238a62c865020ec4cf43c8a78cdde
Varun Venkataraman <vvenkata@adobe.com>
2016-03-12 18:12:18 +1100

    Renaming Lab0 - Lab1

--- switch.sh
+++ switch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "************* L322 - Lab1 - Switch to Lesson 2 ***********"
-git checkout lab1
-echo "************* L322 - Lab1 - End Switch to Lesson 2   ***********"
+echo "************* L322 - Lab2 - Switch to Lesson 2 ***********"
+git checkout lab2
+echo "************* L322 - Lab2 - End Switch to Lesson 2   ***********"
