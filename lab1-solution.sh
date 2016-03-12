0155ae3940cbc36c2e52a6f39d0ab59040a58f01
Varun Venkataraman <vvenkata@adobe.com>
2016-03-12 18:11:18 +1100

    Renaming Lab0 - Lab1

---
+++ lab1-solution.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "************* L322 - Lab0 Solution - Start Initialisation ***********"
+git checkout lab1-solution
+mvn install package -PautoInstallPackage
+echo "************* L322 - Lab0 Solution - End Initialisation  ***********"
