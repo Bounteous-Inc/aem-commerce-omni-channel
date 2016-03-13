768ecc093fbd23a73b238a17f89f4af07cc4287a
Varun Venkataraman <vvenkata@adobe.com>
2016-03-13 11:40:12 +1100

    Lab 2 - Task 1 (Basic)

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "************* L322 - Lab2 - Start Initialisation ***********"
-mvn install package -PautoInstallPackage
+mvn clean install package -PautoInstallPackage
 echo "************* L322 - Lab2 - End Initialisation  ***********"
