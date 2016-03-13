84a11cd42bfd6a3787f44f3d790af4e37fb04c93
Varun Venkataraman <vvenkata@adobe.com>
2016-03-13 13:34:32 +1100

    Lab 2 - Solution

---
+++ lab2-solution.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "************* L322 - Lab2 - Start Initialisation Solution ***********"
+git checkout lab2-solution
+mvn clean install package -PautoInstallPackage
+echo "************* L322 - Lab2 - End Initialisation Solution ***********"
