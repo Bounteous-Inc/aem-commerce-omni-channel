e4be6b319e659ba17a28714f524eb1eb5cd23989
Varun Venkataraman <vvenkata@adobe.com>
2016-03-14 14:15:27 +1100

    Commands to switch to Lab4 Task 2.

--- switch.sh
+++ switch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "************* L322 - Lab3 - Switch to Lab4 ***********"
-git checkout lab4
-echo "************* L322 - Lab3 - End Switch to Lab4   ***********"
+echo "************* L322 - Lab4 - Switch to Lab4-task2 ***********"
+git checkout lab4-task2
+echo "************* L322 - Lab4 - End Switch to Lab4-task2   ***********"
