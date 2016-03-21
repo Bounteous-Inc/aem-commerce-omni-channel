57d9d5ef393eaf8b1a0955337fc5ad34dee9f9ca
Varun Venkataraman <vvenkata@adobe.com>
2016-03-20 17:30:09 -0700

    updated init

--- init.sh
+++ init.sh
@@ -7,9 +7,10 @@ echo "************* Performing Git pull *************"
 git pull
 echo "************* Performing Copy of Maven dependencies to local repository"
 cp -rf external/* ~/.m2/repository
+cp -rf documentation/* /Users/l322/Desktop/
 echo "************* Performing Git checkout *********"
 git checkout lab1
-echo """""""""""""" Performing Auto install package of Lab1 ******"
+echo "************* Performing Auto install package of Lab1 ******"
 mvn clean install -PautoInstallPackage
 echo "************* Copy Presentation, Workbook and Cheatsheets to Desktop"
 cp /Users/l322/Desktop/AEM-Commerce-Lab-WS/aem-commerce-omni-channel/documentation/* /Users/l322/Desktop/
